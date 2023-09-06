2bb439b39c5d03f66ce2231a18aebcb74ee45a57
nekonekko <momoichigodayo+github@gmail.com>
2023-09-06 16:44:37 +0900

    fix: :bug: fix path in conf.py

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -5,7 +5,7 @@
 
 import os, sys
 
-sys.path.insert(0, os.path.abspath("../scegot/"))
+sys.path.insert(0, os.path.abspath("../../scegot/"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
