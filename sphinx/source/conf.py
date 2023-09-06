5e5d8f3c78ad0fdc96de454bdbbef7aa87b4d1d1
nekonekko <momoichigodayo+github@gmail.com>
2023-09-06 14:36:18 +0900

    style: :art: delete blank line in conf.py

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -7,7 +7,6 @@ import os, sys
 
 sys.path.insert(0, os.path.abspath("../scegot/"))
 
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
