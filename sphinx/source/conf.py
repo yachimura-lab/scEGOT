ebd1e649741b4a4ca7fbc0342cab9cd6035e97d3
memento-mogi <tomotomo0412mogi@gmail.com>
2025-04-03 14:17:06 +0900

    feat: 🚀 update to 0.2.6

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath("../../scegot/"))
 project = "scegot"
 copyright = "2023, Toshiaki Yachimura"
 author = "Toshiaki Yachimura"
-release = "0.2.5"
+release = "0.2.6"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
